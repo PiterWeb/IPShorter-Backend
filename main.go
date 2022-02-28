139ad2bc94b4e5a3039084d2f49f06786a762f8a
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2022-02-28 11:41:17 +0100

    Update

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"context"
 	"crypto/tls"
-	"log"
-	"os"
-	"regexp"
-	"time"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	"github.com/teris-io/shortid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/gomail.v2"
+	"log"
+	"os"
+	"regexp"
+	"time"
 )
 
 var ctx = context.TODO()
@@ -22,16 +24,16 @@ var loggerColl = client.Database("databases").Collection("loggers")
 var userColl = client.Database("databases").Collection("users")
 
 type visitor struct {
-	IPs      string
+	IP      string
 	Clicked time.Time
 }
 
 type logger struct {
-	Id        string
-	Url       string
-	ApiKey    string
-	Visitors  []visitor
-	Clicks    int
+	Id       string
+	Url      string
+	ApiKey   string
+	Visitors []visitor
+	Clicks   int
 }
 
 type user struct {
@@ -42,12 +44,12 @@ type user struct {
 func ConnectDB() *mongo.Client {
 
 	godotenv.Load()
-	
+
 	var dbUser string = os.Getenv("MONGO_USER")
 	var dbPass string = os.Getenv("MONGO_PSW")
 	var dbDm string = os.Getenv("MONGO_DM")
 
-	var uri string = "mongodb+srv://" + dbUser + ":" + dbPass + "@"+ dbDm + "/?retryWrites=true&w=majority"
+	var uri string = "mongodb+srv://" + dbUser + ":" + dbPass + "@" + dbDm + "/?retryWrites=true&w=majority"
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 
@@ -65,6 +67,11 @@ func ConnectDB() *mongo.Client {
 
 func createLogger(logger *logger) error {
 	_, err := loggerColl.InsertOne(ctx, &logger)
+	return err
+}
+
+func deleteLogger(id string) error {
+	_, err := loggerColl.DeleteOne(ctx, bson.M{"id": id})
 	return err
 }
 
@@ -112,21 +119,32 @@ func findUserByEmail(email string) (bool, string) {
 	return true, user.ApiKey
 }
 
-// func createAccount() error {
-
-// }
-
 func isEmailValid(e string) bool {
 	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 	return emailRegex.MatchString(e)
 }
 
+func getIP(c *fiber.Ctx) string {
+	if len(c.IPs()) > 0 {
+		return c.IPs()[0]
+	} else {
+		return "Uknown Visitor"
+	}
+}
+
 func main() {
+
+	sid, _ := shortid.New(1, shortid.DefaultABC, 2342)
 
 	var mailPsw string = os.Getenv("MAIL_PSW")
 	var ownMail string = os.Getenv("MAIL")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET, POST, DELETE",
+	}))
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, ownMail, mailPsw)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
@@ -136,7 +154,7 @@ func main() {
 		email := c.FormValue("email")
 
 		if !isEmailValid(email) {
-			return c.Status(400).JSON(&fiber.Map{"message": "Invalid email"})
+			return c.Status(300).JSON(&fiber.Map{"message": "Invalid email"})
 		}
 
 		if ok, apiKey := findUserByEmail(email); ok {
@@ -151,7 +169,7 @@ func main() {
 				panic(err)
 			}
 
-			return c.Status(400).JSON(&fiber.Map{"error": "User already have an apiKey , but we resent it to you by email"})
+			return c.Status(300).JSON(&fiber.Map{"message": "User already have an apiKey , but we resent it to you by email"})
 		}
 
 		apiKey := uuid.New().String()
@@ -168,8 +186,7 @@ func main() {
 
 		userColl.InsertOne(context.TODO(), &user{Email: email, ApiKey: apiKey})
 
-		return c.Status(200).JSON(&fiber.Map{"message": "ApiKey sent to your email"})
-
+		return c.JSON(&fiber.Map{"message": "ApiKey sent to your email"})
 	})
 
 	app.Post("/api/createLogger/:apiKey", func(c *fiber.Ctx) error {
@@ -178,39 +195,39 @@ func main() {
 		url := c.FormValue("url")
 
 		if url == "" {
-			return c.Status(400).JSON(&fiber.Map{"error": "url is required"})
+			return c.Status(300).JSON(&fiber.Map{"message": "url is required"})
 		} else if exLogger, _ := findLoggerByUrl(url); exLogger != nil {
 
-			if (exLogger.ApiKey == apiKey) {
-				return c.Status(400).JSON(&exLogger)
-			}	
+			if exLogger.ApiKey == apiKey {
+				return c.JSON(&exLogger)
+			}
 
 		}
 
 		if ok, _ := findUserByApiKey(apiKey); !ok {
-			return c.Status(400).JSON(&fiber.Map{"error": "apiKey does not exist", "apiKey": apiKey})
+			return c.Status(300).JSON(&fiber.Map{"message": apiKey + " does not exist as an apiKey"})
 		}
 
-		idLogger := uuid.New().String()
+		idLogger, _ := sid.Generate()
 
 		urlLogger := &logger{
-			Id:        idLogger,
-			Url:       url,
-			ApiKey:    apiKey,
+			Id:     idLogger,
+			Url:    url,
+			ApiKey: apiKey,
 		}
 
 		if err := createLogger(urlLogger); err != nil {
-			return c.Status(400).JSON(&fiber.Map{"error": "error creating logger"})
+			return c.Status(300).JSON(&fiber.Map{"message": "error creating logger"})
 		}
 
 		type response struct {
-			Id        string
-			Url       string
+			Id  string
+			Url string
 		}
 
 		res := &response{
-			Id:        idLogger,
-			Url:       url,
+			Id:  idLogger,
+			Url: url,
 		}
 
 		return c.JSON(res)
@@ -225,21 +242,62 @@ func main() {
 		cur, err := loggerColl.Find(ctx, bson.M{"apikey": apiKey})
 
 		if err != nil {
-			return c.Status(400).JSON(&fiber.Map{"error": "error getting loggers"})
+			return c.Status(300).JSON(&fiber.Map{"message": "error getting loggers"})
 		}
 
 		for cur.Next(ctx) {
 			var logger logger
 			err := cur.Decode(&logger)
 			if err != nil {
-				return c.Status(400).JSON(&fiber.Map{"error": "error getting loggers"})
+				return c.Status(300).JSON(&fiber.Map{"message": "error getting loggers"})
 			}
 
 			loggers = append(loggers, logger)
 		}
 
-		return c.JSON(loggers)
+		return c.Status(200).JSON(loggers)
 
+	})
+
+	app.Get("/api/getLogger/:apiKey/:id", func(c *fiber.Ctx) error {
+
+		apiKey := c.Params("apiKey")
+		id := c.Params("id")
+
+		logger, err := findLoggerById(id)
+
+		if err != nil {
+			return c.Status(300).JSON(&fiber.Map{"message": "error getting loggers"})
+		}
+
+		if logger.ApiKey == apiKey {
+			return c.JSON(logger)
+		}
+
+		return c.Status(300).JSON(&fiber.Map{"message": "error getting loggers"})
+
+	})
+
+	app.Delete("/api/deleteLogger/:apiKey/:id", func(c *fiber.Ctx) error {
+
+		apiKey := c.Params("apiKey")
+		id := c.Params("id")
+
+		logger, err := findLoggerById(id)
+
+		if err != nil {
+			return c.Status(300).JSON(&fiber.Map{"message": "error getting loggers"})
+		}
+
+		if logger.ApiKey == apiKey {
+			if err := deleteLogger(id); err != nil {
+				return c.Status(300).JSON(&fiber.Map{"message": "error deleting logger"})
+			}
+
+			return c.JSON(&fiber.Map{"message": "logger deleted"})
+		}
+
+		return c.Status(300).JSON(&fiber.Map{"message": "error getting loggers"})
 	})
 
 	app.Get("/:id", func(c *fiber.Ctx) error {
@@ -249,16 +307,16 @@ func main() {
 		logger, err := findLoggerById(id)
 
 		if err != nil {
-			return c.Status(400).JSON(&fiber.Map{"error": "error getting the destination url"})
+			return c.Status(300).JSON(&fiber.Map{"message": "error getting the destination url"})
 		}
 
 		logger.Clicks++
 		loggerColl.UpdateOne(ctx, bson.M{"id": id}, bson.M{"$set": bson.M{"clicks": logger.Clicks}})
 
-		userIP := c.IPs()[0]
+		userIP := getIP(c)
 
 		visitor := visitor{
-			IPs:      userIP,
+			IP:      userIP,
 			Clicked: time.Now(),
 		}
 
@@ -270,29 +328,10 @@ func main() {
 
 	})
 
-	app.Get("/api/dashboard/:apiKey/:id", func(c *fiber.Ctx) error {
-
-		apiKey := c.Params("apiKey")
-		id := c.Params("id")
-
-		logger , err := findLoggerById(id)
-
-		if err != nil {
-			return c.Status(400).JSON(&fiber.Map{"error": "error getting loggers"})
-		}
-
-		if logger.ApiKey == apiKey {
-			return c.JSON(logger)
-		}
-
-		return c.Status(400).JSON(&fiber.Map{"error": "error getting loggers"})
-
-	})
-
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		port = "8080"
+		port = "8081"
 	}
 
 	err := app.Listen(":" + port)
